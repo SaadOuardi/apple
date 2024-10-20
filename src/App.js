d3eb4ea309d6ddc2ae36d7c6342fc7bccb3de64c
SaadOuardi <saad.ouardi.developer@gmail.com>
2024-10-20 23:21:16 +0200

    new version based on links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Customization from './pages/Customization';
 import BurgerCustomization from './pages/BurgerCustomization';
 
-const App = () =>{
+const App = () => {
   return (
-    <>
-    <Router>
+    <Router basename='/apple'>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/customization" element={<Customization />} />
         <Route path="/product" element={<BurgerCustomization />} />
       </Routes>
     </Router>
-    </>
   );
 };
 
